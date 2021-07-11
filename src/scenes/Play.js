41f70b2c8d60ffc79130de9ec6fb663c1b6a26c8
vderbier <victor.derbier@gmail.com>
2021-07-11 13:39:49 -0700

    added assets to play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,10 +4,13 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('player', './assets/walkerRound.png');
-    }
+        this.load.image('player', './assets/walker.png');
+        this.load.image('background', './assets/background.png');
+        this.load.image('ground', './assets/ground.png');
+        }
 
     create() {
-        this.add.sprite(240, 420, 'player');
-    }
+        this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+        this.add.sprite(game.config.height/4, game.config.width/4, 'player').setOrigin(0, 0);
+        }
 }
