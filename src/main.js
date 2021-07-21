766dbc4a4396779f0c701e39e542ebcae581c609
Harsha <srmadala@ucsc.edu>
2021-07-21 12:42:24 -0700

    added credits scene (doesn't have anything in it)

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ let config = {
             }
         }
     },
-    scene: [ Menu, Level_1, Level_2, Level_3, Level_4, Level_5, Level_6, Level_7, Level_8, GameOver ]
+    scene: [ Menu, Level_1, Level_2, Level_3, Level_4, Level_5, Level_6, Level_7, Level_8, GameOver, Credits ]
 }
 
 let game = new Phaser.Game(config); 
